f05269647adce968e86233932612c88232789b78
Breno <breno.oleranos@aluno.senai>
2026-09-03 11:12:47 -0300

    Alterações aula 03/09

---
+++ src/telas/jRaddioButtonFeminino.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package telas;
+
+/**
+ *
+ * @author Aluno
+ */
+class jRaddioButtonFeminino {
+
+    static boolean isSelected() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
